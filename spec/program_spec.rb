89649795bb9b5af1055bb0e1eeacbb8b2c606391
Emma <emma_williams1@hotmail.co.uk>
2014-04-07 12:22:12 +0100

    added bad path test to first method

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -4,11 +4,16 @@ describe "FizzBuzz" do
 	#This is where we will describe our rules
 	#we now need a true test and a false test for every method we will write
 
-	it "divide_by_three?" do 
+	it "Does three divide by three?" do 
 		#it should be readable for the programmer-not the method. 
 		#does the number divide by three, do is opening the block as there is a block of code here
 	expect(divide_by_three?(3)).to be_true
 	#What to expect. (Method with argument in () for readability. .to = method that evaluates be.true=to be true)
+	end
+
+	it "Does one divide by three?" do
+		expect(divide_by_three?(1)).to be_false
+		#this evaluates the bad path-expecting to be false
 	end
 
 
