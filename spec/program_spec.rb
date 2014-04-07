e412382d7ec9869f60e04d7d1a564b775933b8fe
Emma <emma_williams1@hotmail.co.uk>
2014-04-07 12:03:11 +0100

    With first test and fail

---
+++ spec/program_spec.rb
@@ -0,0 +1,29 @@
+require 'program'
+#I need this program
+describe "FizzBuzz" do 
+	#This is where we will describe our rules
+	#we now need a true test and a false test for every method we will write
+
+	it "divide_by_three?" do 
+		#it should be readable for the programmer-not the method. 
+		#does the number divide by three, do is opening the block as there is a block of code here
+	expect(divide_by_three?(3)).to be.true
+	#What to expect. (Method with argument in () for readability. .to = method that evaluates be.true=to be true)
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	end
